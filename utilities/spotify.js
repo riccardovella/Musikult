c62f2b629b587a1e21a55cf63a551ff9d06baadb
riccardovella <vella.1806809@studenti.uniroma1.it>
2020-09-14 16:43:49 +0200

    fix

--- utilities/spotify.js
+++ utilities/spotify.js
@@ -110,6 +110,9 @@ function searchArtist(access_token, artist, func, song = null) {
 
 function searchSong(access_token, song, func, artist = null) {
 
+    song = song.replace("&", " ");
+    artist = artist.replace("&", " ");
+
     var options = {
         url: "https://api.spotify.com/v1/search?type=track&" + querystring.stringify({ q: song + " " + artist }),
         headers : {
